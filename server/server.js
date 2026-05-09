f75f24a941d7e76200f2a27428fcce3e4c808043
Dylan Calle <dylan.calle.dev@gmail.com>
2026-05-08 22:18:58 -0400

    feat: add boilerplate for server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = 4800;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const UserSchema = new mongoose.Schema({
+  id: Number,
+  username: String,
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const User = mongoose.model("User", UserSchema, "users");
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Database error",
+    });
+  }
 });
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+
+  console.log("Mongo connected");
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+} catch (error) {
+  console.error(error);
+}
